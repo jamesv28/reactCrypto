3db4946020c0096986beb67979dd977e60972046
James Volmert <james.volmert@gmail.com>
2019-01-24 11:57:20 -0700

    worked on compact tiles

--- src/dashboard/PriceTile.js
+++ src/dashboard/PriceTile.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {SelectableTile} from '../shared/Tile';
-import {fontSize3} from '../shared/Styles';
+import {fontSize3,fontSizeBig} from '../shared/Styles';
 import {CoinHeaderGridStyled} from '../settings/CoinHeaderGrid';
+import { numberFormat } from 'highcharts';
 
 const numberFormatter = number => {
     return +(number + '').slice(0,7);
 }
+const TickerPrice = styled.div`
+    ${fontSizeBig}
+`
+const JustifyRight = styled.div`
+    justify-self: right;
+`
+const JustifyLeft = styled.div`
+    justify-self: left;
+`
+
+const PercentChange = styled.div`
+    color: #76EEC6;
+    ${props => props.red && css`
+        color: #FF3030;    
+    `}
+`
+function ChangePercent({data}) {
+    return (
+        <JustifyRight>
+            <PercentChange red={data.CHANGEPCT24HOUR < 0}>
+                {numberFormatter(data.CHANGEPCT24HOUR)}
+            </PercentChange>
+        </JustifyRight>
+    )
+}
 const PriceItemStyled = styled(SelectableTile)`
     ${props => props.compact && css `
+        display: grid;
+        grid-gap: 5px;
         ${fontSize3}
+        grid-template-columns: repeat(3, 1fr);
     `}
 `
 
@@ -18,19 +47,38 @@ function PriceTile({sym, data}) {
         <PriceItemStyled>
             <CoinHeaderGridStyled>
                 <div>{sym}</div>
-                <div>{numberFormatter(data.CHANGEPCT24HOUR)}</div>
+                <ChangePercent data={data}/>
             </CoinHeaderGridStyled>
+            <TickerPrice>
+                ${numberFormat(data.PRICE)}
+            </TickerPrice>
         </PriceItemStyled>
     )
 }
+
+function PriceTileCompact({sym, data}) {
+    return (
+        <PriceItemStyled compact>
+            <JustifyLeft>
+                {sym}
+            </JustifyLeft>
+            <ChangePercent data={data}/>
+            <div>
+                ${numberFormat(data.PRICE)}
+            </div>
+        </PriceItemStyled>
+    )
+}
+
 export default function({price, index}) {
 
     let sym = Object.keys(price)[0];
     let data = price[sym]['USD'];
+    let TileClass = index < 5 ? PriceTile : PriceTileCompact;
 
     return (
-        <PriceTile sym={sym} data={data}>
-            {sym} {data.PRICE}
-        </PriceTile>
+        <TileClass sym={sym} data={data}>
+            
+        </TileClass>
     )
 }
